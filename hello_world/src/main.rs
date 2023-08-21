f96b00c6db0c62b9cffce6f38c28a25ce3468e45
hasegawa-101 <hasegawa@101de-sign.com>
2023-08-21 22:27:54 +0900

    コミット漏れ

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    //    let x = 5;
+    //    x = 10;
+    //    変数はimmutableのため上記はエラーになる。
+    let mut x = 5;
+    println!("{}", x);
+    x = 10;
+    println!("{},", x);
 }
