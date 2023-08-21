1e95ad4b6d74207b3a51d58ad08b2ad41dc58e44
hasegawa-101 <hasegawa@101de-sign.com>
2023-08-22 01:22:40 +0900

    HTTPリクエストを試した

---
+++ network/src/main.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+
+use reqwest;
+
+fn do_something() {}
+
+#[tokio::main]
+// Result<(), Box<dyn Error>>型は、
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://www.rustinaction.com/";
+    let response = reqwest::get(url).await?;
+
+    let content = response.text().await?;
+    print!("{}", content);
+
+    for i in 0..100 {
+        do_something();
+    }
+
+    Ok(())
+}
