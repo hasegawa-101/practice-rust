8d3e1bc6b7ea55e2e5310a13b58f139ed2fac69f
hasegawa-101 <hasegawa@101de-sign.com>
2023-08-21 23:59:14 +0900

    番地を調べてみた

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,6 @@
+const POINT: u32 = 100_000;
+// 100_000を100000としても10_0000としてもエラーは出ない（？）
+
 fn main() {
     println!("Hello, world!");
     //    let x = 5;
@@ -7,4 +10,18 @@ fn main() {
     println!("{}", x);
     x = 10;
     println!("{},", x);
+
+    println!("ポイントは {} です。", POINT);
+
+    println!("{}", usize::BITS);
+    println!("メモリアドレスの番地は {:p} です。", &POINT);
+
+    let i1: u64 = 1;
+    let i2: u64 = 2;
+
+    println!("スタックアドレスは {:p} です。", &i1);
+    println!("スタックアドレスは {:p} です。", &i2);
+    //    結果
+    //    スタックアドレスは 0x16bdaade0 です。
+    //    スタックアドレスは 0x16bdaade8 です。
 }
